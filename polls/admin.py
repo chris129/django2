15e1ccd359916f71b93f4ee1c7e281b34f100637
chrisli129 <chrisli129@gmail.com>
2018-04-08 16:30:44 +0800

    第七部分-侧边栏和搜索框

--- polls/admin.py
+++ polls/admin.py
@@ -13,6 +13,10 @@ class QuestionAdmin(admin.ModelAdmin):
     #以列的形式展示
     list_display = ('question_text','pub_date','was_published_recently')
     inlines = [ChoiceInline]
+    #添加了一个“过滤器”侧边栏，允许人们以 pub_date 字段来过滤列表
+    list_filter = ['pub_date']
+    #增加一个搜索框。当输入待搜项时，Django 将搜索 question_text 字段
+    search_fields = ['question_text']
 
 
 #创建一个模型后台类，接着将其作为第二个参数传给 admin.site.register() ——在你需要修改模型的后台管理选项时这么做。
